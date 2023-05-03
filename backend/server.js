2a9711a5e6f44e78d74d561b6488a9faa7fcc86f
Yash Chavda <yashchavda360@gmail.com>
2023-05-03 05:21:28 -0400

    initial rest API for goals

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,10 @@ const dotenv = require("dotenv").config();
 const port = process.env.PORT || 3000;
 const { errorHandler } = require("./middleware/errorMiddleware");
 
+const connectDB = require("./config/db");
+
+connectDB();
+
 const app = express();
 
 app.use(express.json());
